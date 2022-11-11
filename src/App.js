0bdd4a715bc625c1876403e365e27b33e360bbe9
time-to-program <raviteja.venkatesh@costbo.com>
2022-11-11 13:28:55 +0530

    otp countdown timer in reatc js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [otp, setOtp] = useState("");
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(30);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(interval);
+        } else {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seconds]);
+
+  const resendOTP = () => {
+    setMinutes(1);
+    setSeconds(30);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <h4>Verify OTP</h4>
+
+        <input
+          placeholder="Enter OTP"
+          value={otp}
+          onChange={({ target }) => {
+            setOtp(target.value);
+          }}
+        />
+
+        <div className="countdown-text">
+          {seconds > 0 || minutes > 0 ? (
+            <p>
+              Time Remaining: {minutes < 10 ? `0${minutes}` : minutes}:
+              {seconds < 10 ? `0${seconds}` : seconds}
+            </p>
+          ) : (
+            <p>Didn't recieve code?</p>
+          )}
+
+          <button
+            disabled={seconds > 0 || minutes > 0}
+            style={{
+              color: seconds > 0 || minutes > 0 ? "#DFE3E8" : "#FF5630",
+            }}
+            onClick={resendOTP}
+          >
+            Resend OTP
+          </button>
+        </div>
+
+        <button className="submit-btn">SUBMIT</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
