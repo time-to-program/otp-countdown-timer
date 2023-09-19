63318ef5147e20aa94ed75d4f515a4475d89bfa5
TimeToProgram <timeforprogramming@gmail.com>
2023-09-19 18:28:17 +0530

    OTP Countdown Timer in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  // State variables to manage OTP input, minutes, and seconds
   const [otp, setOtp] = useState("");
   const [minutes, setMinutes] = useState(1);
-  const [seconds, setSeconds] = useState(30);
+  const [seconds, setSeconds] = useState(59);
 
   useEffect(() => {
+    // Function to handle the countdown logic
     const interval = setInterval(() => {
+      // Decrease seconds if greater than 0
       if (seconds > 0) {
         setSeconds(seconds - 1);
       }
 
+      // When seconds reach 0, decrease minutes if greater than 0
       if (seconds === 0) {
         if (minutes === 0) {
+          // Stop the countdown when both minutes and seconds are 0
           clearInterval(interval);
         } else {
+          // Reset seconds to 59 and decrease minutes by 1
           setSeconds(59);
           setMinutes(minutes - 1);
         }
       }
-    }, 1000);
+    }, 1000); // Run this effect every 1000ms (1 second)
 
     return () => {
+      // Cleanup: stop the interval when the component unmounts
       clearInterval(interval);
     };
-  }, [seconds]);
+  }, [seconds]); // Re-run this effect whenever 'seconds' changes
 
+  // Function to resend OTP
   const resendOTP = () => {
     setMinutes(1);
     setSeconds(30);
@@ -36,6 +45,7 @@ function App() {
       <div className="card">
         <h4>Verify OTP</h4>
 
+        {/* Input field for entering OTP */}
         <input
           placeholder="Enter OTP"
           value={otp}
@@ -45,15 +55,22 @@ function App() {
         />
 
         <div className="countdown-text">
+          {/* Display countdown timer if seconds or minutes are greater than 0 */}
+          
           {seconds > 0 || minutes > 0 ? (
             <p>
-              Time Remaining: {minutes < 10 ? `0${minutes}` : minutes}:
-              {seconds < 10 ? `0${seconds}` : seconds}
+              Time Remaining:{" "}
+              <span style={{ fontWeight: 600 }}>
+                {minutes < 10 ? `0${minutes}` : minutes}:
+                {seconds < 10 ? `0${seconds}` : seconds}
+              </span>
             </p>
           ) : (
-            <p>Didn't recieve code?</p>
+            // Display if countdown timer reaches 0
+            <p>Didn't receive code?</p>
           )}
 
+          {/* Button to resend OTP */}
           <button
             disabled={seconds > 0 || minutes > 0}
             style={{
@@ -65,6 +82,7 @@ function App() {
           </button>
         </div>
 
+        {/* Button to submit OTP */}
         <button className="submit-btn">SUBMIT</button>
       </div>
     </div>
@@ -72,5 +90,3 @@ function App() {
 }
 
 export default App;
-
-
